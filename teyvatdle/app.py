b399c7580f1425a8bf20e41646c1f067ff7dec96
wesleyyxie <wesleyxie05@gmail.com>
2024-09-30 23:14:35 -0400

    added routes

--- teyvatdle/app.py
+++ teyvatdle/app.py
@@ -3,9 +3,29 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route("/spy")
+def spy():
+    return render_template("spy_mode.html")
+
+
+@app.route("/ability")
+def ability():
+    return render_template("ability_mode.html")
+
+
+@app.route("/voiceline")
+def voiceline():
+    return render_template("voiceline_mode.html")
+
+
+@app.route("/classic")
+def classic():
+    return render_template("classic_mode.html")
+
+
 @app.route("/")
 def hello_world():
-    return render_template("base.html")
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
