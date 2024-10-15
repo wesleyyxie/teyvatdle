55c03f465e6a73f210083699a11e5830f0f0ea34
wesleyyxie <wesleyxie05@gmail.com>
2024-10-15 15:37:08 -0400

    auto correct works with second word of name

--- teyvatdle_app/static/scripts/auto-complete.js
+++ teyvatdle_app/static/scripts/auto-complete.js
@@ -19,6 +19,7 @@ function autocomplete(inp, arr) {
         for (i = 0; i < arr.length; i++) {
           /*check if the item starts with the same letters as the text field value:*/
           characterName = arr[i]["name"]
+          characterNameParts = characterName.split(" ")
           if (characterName.substr(0, val.length).toUpperCase() == val.toUpperCase()) {
             /*create a DIV element for each matching element:*/
             container = document.createElement("DIV")
@@ -29,8 +30,35 @@ function autocomplete(inp, arr) {
             b = document.createElement("DIV");
             b.setAttribute("class", "flex p-[5px] h-[50px] border-b items-center")
             /*make the matching letters bold:*/
-            b.innerHTML = "<strong>" + characterName.substr(0, val.length) + "</strong>";
-            b.innerHTML += characterName.substr(val.length);
+            b.innerHTML = "<strong>" + characterName.substr(0, val.length).replace(" ", "&nbsp;") + "</strong>";
+            b.innerHTML += characterName.substr(val.length).replace(" ", "&nbsp;");
+            /*insert a input field that will hold the current array item's value:*/
+            b.innerHTML += "<input type='hidden' value='" + characterName + "'>";
+            /*execute a function when someone clicks on the item value (DIV element):*/
+            container.addEventListener("click", function(e) {
+                /*insert the value for the autocomplete text field:*/
+                inp.value = this.getElementsByTagName("input")[0].value;
+                /*close the list of autocompleted values,
+                (or any other open lists of autocompleted values:*/
+                closeAllLists();
+            });
+            container.appendChild(icon)
+            container.appendChild(b);
+            a.appendChild(container)
+          }
+          if ((characterNameParts.length > 1) && (characterNameParts[1].substr(0, val.length).toUpperCase() == val.toUpperCase())) {
+            /*create a DIV element for each matching element:*/
+            container = document.createElement("DIV")
+            container.setAttribute("class", "flex cursor-pointer items-center p-[5px] hover:bg-[#e9e9e9]")
+            icon = document.createElement("DIV")
+            icon.style.backgroundImage = `url('/static/images/character_icons/${arr[i]["id"].toLowerCase()}.png')`
+            icon.setAttribute("class", "h-[50px] w-[50px] bg-contain bg-no-repeat bg-center mr-[2px]")
+            b = document.createElement("DIV");
+            b.setAttribute("class", "flex p-[5px] h-[50px] border-b items-center")
+            /*make the matching letters bold:*/
+            b.innerHTML = characterNameParts[0]
+            b.innerHTML += "<strong>&nbsp;" + characterNameParts[1].substr(0, val.length) + "</strong>";
+            b.innerHTML += characterNameParts[1].substr(val.length);
             /*insert a input field that will hold the current array item's value:*/
             b.innerHTML += "<input type='hidden' value='" + characterName + "'>";
             /*execute a function when someone clicks on the item value (DIV element):*/
