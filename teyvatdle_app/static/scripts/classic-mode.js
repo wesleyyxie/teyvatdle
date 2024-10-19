e2ef0d2709a8ea3ffa775d6997cdcdd95d126240
Jason Lam <ssjjlam@gmail.com>
2024-10-19 00:41:59 -0400

    added other modes buttons and tool tip(doesn't work properly yet

--- teyvatdle_app/static/scripts/classic-mode.js
+++ teyvatdle_app/static/scripts/classic-mode.js
@@ -84,28 +84,90 @@ function submitGuess(e) {
     }
 }
 
-// Congrats message 
 function displayCongratulatoryMessage(tries) {
     const congratsMessageElement = document.getElementById("congrats_message");
     congratsMessageElement.innerHTML = ""; 
-    let message = "";
-    message = "Congrats!";
+    let message = "Congrats!";
 
     // Congrats text
     const congratsText = document.createElement("div");
     congratsText.innerText = message;
-    congratsText.classList.add("font-bold", "text-2xl","text-center"); // font size
+    congratsText.classList.add("font-bold", "text-3xl", "text-center"); // Increased font size
 
-    // number of tries text
+    // Number of tries text
     const triesText = document.createElement("div");
     triesText.innerText = tries < 2 ? "You guessed it in 1 try!" : `You guessed it in ${tries} tries!`;
-    triesText.classList.add("text-lg", "text-center"); // font size
+    triesText.classList.add("text-lg", "text-center");
 
+    // Other Modes header
+    const modesHeader = document.createElement("div");
+    modesHeader.innerText = "Other Modes";
+    modesHeader.classList.add("font-bold", "text-xl", "text-center", "mt-4"); 
+
+    // Function to show tooltip
+    function showTooltip(button, text) {
+        const tooltip = document.getElementById("tooltip");
+        tooltip.innerText = text;
+
+        const buttonRect = button.getBoundingClientRect(); // Get the button's position
+        tooltip.style.left = `${buttonRect.left + (buttonRect.width / 2) - (tooltip.offsetWidth / 2)}px`; // Center tooltip above button
+        tooltip.style.top = `${buttonRect.top - tooltip.offsetHeight - 5}px`; // Position above button with some space
+        tooltip.classList.remove("hidden");
+    }
+
+    // Function to hide tooltip
+    function hideTooltip() {
+        const tooltip = document.getElementById("tooltip");
+        tooltip.classList.add("hidden");
+    }
+
+    // Create button container
+    const buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("flex", "gap-4", "justify-center", "mt-2"); // Adjust margin 
+
+    // Voiceline Mode button
+    const voicelineButton = document.createElement("img");
+    voicelineButton.src = "/static/images/buttons/voiceline_1.png"; 
+    voicelineButton.alt = "Voiceline Mode";
+    voicelineButton.classList.add("w-[50px]", "h-[50px]", "cursor-pointer", "hover:scale-button"); // Adjust size 
+    voicelineButton.onclick = () => window.location.href = "voiceline";
+
+    voicelineButton.addEventListener("mouseenter", () => showTooltip(voicelineButton, "Voiceline"));
+    voicelineButton.addEventListener("mouseleave", hideTooltip);
+
+
+    // Ability Mode button
+    const abilityButton = document.createElement("img");
+    abilityButton.src = "/static/images/buttons/ability_1.png"; 
+    abilityButton.alt = "Ability Mode";
+    abilityButton.classList.add("w-[50px]", "h-[50px]", "cursor-pointer", "hover:scale-button"); // Adjust size 
+    abilityButton.onclick = () => window.location.href = "ability";
+
+    abilityButton.addEventListener("mouseenter", () => showTooltip(abilityButton, "Ability"));
+    abilityButton.addEventListener("mouseleave", hideTooltip);
+
+    // Spy Mode button
+    const spyButton = document.createElement("img");
+    spyButton.src = "/static/images/buttons/spy_1.png"; 
+    spyButton.alt = "Spy Mode";
+    spyButton.classList.add("w-[50px]", "h-[50px]", "cursor-pointer", "hover:scale-button"); // Adjust size 
+    spyButton.onclick = () => window.location.href = "spy";
+
+    spyButton.addEventListener("mouseenter", () => showTooltip(spyButton, "Spy"));
+    spyButton.addEventListener("mouseleave", hideTooltip);
+
+    buttonContainer.appendChild(voicelineButton);
+    buttonContainer.appendChild(abilityButton);
+    buttonContainer.appendChild(spyButton);
+
+    // Append everything to the congrats message element
     setTimeout(() => {
         congratsMessageElement.appendChild(congratsText);
         congratsMessageElement.appendChild(triesText);
+        congratsMessageElement.appendChild(modesHeader); 
+        congratsMessageElement.appendChild(buttonContainer);
         congratsMessageElement.classList.remove("hidden");
-        congratsMessageElement.classList.add("text-white", "bg-green-800", "border-4", "border-green-600", "p-4", "mt-4", "rounded");
+        congratsMessageElement.classList.add("text-white", "bg-green-800", "border-4", "border-green-600", "p-6", "mt-4", "rounded"); // Increased padding
         congratsMessageElement.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
     }, 2200); // Delay
 }
