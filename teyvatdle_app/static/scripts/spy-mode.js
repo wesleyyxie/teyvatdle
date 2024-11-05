c5f52064413553bfeed6890eb4915e3254467a34
JasonLam111222 <157567859+JasonLam111222@users.noreply.github.com>
2024-11-04 22:17:19 -0500

    spy mode

--- teyvatdle_app/static/scripts/spy-mode.js
+++ teyvatdle_app/static/scripts/spy-mode.js
@@ -1,46 +1,152 @@
-{% extends "base.html" %}
-{% block gamemode %}
-<script src="{{ url_for('static',filename='/scripts/auto-complete.js') }}"></script>
-<script src="{{ url_for('static',filename='/scripts/spy-mode.js') }}"></script>
-<div class="flex flex-col justify-center items-center gap-y-[10px]">
-    <div class="flex flex-col items-center mt-[20px] h-auto w-[300px] rounded-lg" style="background-color: rgba(39, 52, 63, 0.8); border: 3px solid rgb(120, 120, 120); box-sizing: border-box;">
-        <div class="flex flex-col items-center text-white p-[15px] text-center font-bold mt-[15px]">
-        Which character does this splash art belong to?
-        <!-- Placeholder for the splash image -->
-        <div id="splash-icon" class="h-[192px] w-[192px] bg-no-repeat bg-center bg-cover" style="image-rendering: pixelated;"></div>
-        </div>
-        <span id="clue_countdown" class="text-white text-xs font-bold pb-[25px]">Clues in 5 tries</span>
-        <span id="name_clue" class="hidden text-white font-bold pb-[25px]"></span>
-    </div>
-    <span id="show_character_data"></span>
-    
-    <form autocomplete="off" class="flex w-[300px] items-center relative inline-block" id="guess-form">
-        <div class="autocomplete w-[300px]">
-            <input id="guess" style="background-image: url(&quot;/static/images/input.png&quot;); background-size: 300px 58px;" class="bg-transparent bg-no-repeat outline-none text-white w-[300px] pl-[25px] p-[18px]" type="text" name="guess-character" placeholder="Character">
-        </div>
-        <input id="submit" value='' style="background-image: url(&quot;/static/images/buttons/submit_button.png&quot;); background-size: 40px 40px;" class="absolute hover:brightness-110 right-[10px] bg-no-repeat bg-center bg-transparent aspect-[1] h-[40px] ml-[10px] cursor-pointer" type="submit">
-    </form>
-    <div id="results"></div>
-    
-    <div id="congrats_message" class="hidden flex flex-col items-center text-white bg-green-800 border-4 border-green-600 p-6 mt-4 rounded-lg">
-        <div class="font-bold text-3xl text-center">Congrats!</div>
-        <div id="tried-text" class="text-lg text-center">"You guessed it in 1 try!"</div>
-        <div class="font-bold text-xl text-center mt-4">Next Mode</div>
-        <div class="bg-no-repeat hover:scale-button cursor-pointer bg-center aspect-[173/50] w-[173px]" onclick="location.href='/spy'" style="background-image: url(&quot;/static/images/buttons/spy_button_short.png&quot;); background-size: 173px 50px;"></div>
-        
-        <div class="font-bold text-xl text-center mt-4">Other Modes</div>
-        <div class="flex gap-4 justify-center mt-2">
-            <div class="gamemode-button bg-no-repeat w-[50px] h-[50px] cursor-pointer hover:scale-button" onclick="location.href='/classic'" style="background-image: url(&quot;/static/images/buttons/classic_1.png&quot;); background-size: 50px 50px;">
-                <span class="gamemode-label">Classic Mode</span>
-            </div>
-            <div class="gamemode-button bg-no-repeat w-[50px] h-[50px] cursor-pointer hover:scale-button" onclick="location.href='/voiceline'" style="background-image: url(&quot;/static/images/buttons/voiceline_1.png&quot;); background-size: 50px 50px;">
-                <span class="gamemode-label">Voiceline Mode</span>
-            </div>
-            <div class="gamemode-button bg-no-repeat w-[50px] h-[50px] cursor-pointer hover:scale-button" onclick="location.href='/spy'" style="background-image: url(&quot;/static/images/buttons/spy_1.png&quot;); background-size: 50px 50px;">
-                <span class="gamemode-label">Spy Mode</span>
-            </div>
-        </div>
-    </div>
-</div>
+var charactersInfoData = null;   
+var answerData = null;
+let tries = 0;
 
-{% endblock %}
+function createBlankRow() {
+    rowContainer = document.createElement('div');
+    rowContainer.classList.add("flex", "text-white", "font-bold", "text-[13px]", "flex-row", "gap-x-[12px]", "mb-[10px]");
+    
+    // Create one box for the character icon and result (red or green)
+    categoryDiv = document.createElement('div');
+    categoryDiv.classList.add("flex", "w-[270px]", "h-[130px]", "border", "justify-center", "border-white", "shadow-[inset_0_4px_6px_rgba(0,0,0,0.5)]", "shadow-[0_4px_6px_rgba(0,0,0,0.5)]");
+
+    categoryDiv.id = 'guess_result';  // Single box for the result
+    rowContainer.appendChild(categoryDiv);
+    
+    return rowContainer;
+}
+
+// Checks whether or not the guess was correct or not and changes boxes accordingly
+function checkGuess(guessData, row) {
+    const result_element = row.querySelector("#guess_result");
+
+    // Display the character icon in the result box
+    placeIcon(result_element, guessData);
+    // Check if the guessed character is correct
+    if (guessData["name"].toLowerCase() === answerData["name"].toLowerCase()) {   
+        result_element.classList.add('bg-green');  // Turn green if correct
+        return true;
+    } else {
+        result_element.classList.add('bg-red');  // Turn red if incorrect
+        return false;
+    }
+}
+
+// Places the character icon
+function placeIcon(iconElement, guessData) {
+    console.log("Now placing icon");
+    let imageName;
+
+    // Assuming icons are named using character names
+    imageName = guessData["id"].toLowerCase().replace(/\s+/g, '-');
+
+    const imageUrl = `/static/images/character_icons/${imageName}.png`;
+    console.log("Image URL:", imageUrl);
+
+    iconElement.style.backgroundImage = `url('${imageUrl}')`;
+    iconElement.style.backgroundSize = '75px 75px';
+    iconElement.style.backgroundPosition = 'center 10px';
+    iconElement.style.backgroundRepeat = 'no-repeat';
+
+    spanElement = document.createElement('span')
+    spanElement.classList.add('mt-[90px]','bottom-[8px]', 'text-lg')
+    spanElement.innerText = guessData["name"]
+    iconElement.appendChild(spanElement)
+}
+
+// Handles submission of guess
+function submitGuess(e) {
+    e.preventDefault();  // Prevent form submission
+    let inputElement = document.getElementById("guess");
+    let guess = inputElement.value;
+    let resultsContainer = document.getElementById('results');
+    let clueCountdown = document.getElementById('clue_countdown')
+    let guessData = null;
+    let gameOver = false;
+
+    // Loop through the characters to find the guessed character
+    for (let i = 0; i < charactersInfoData.length; i++) {
+        let currentCharacter = charactersInfoData[i];
+        console.log(currentCharacter)
+        console.log(guess)
+        if (currentCharacter["name"].toLowerCase() === guess.toLowerCase()) {
+            guessData = currentCharacter;
+            tries++;
+            break;
+        }
+    }
+
+    if (guessData) {
+        const row = createBlankRow();
+
+        if (checkGuess(guessData, row)) {
+            displayCongratulatoryMessage(tries);  
+            gameOver = true;
+        }
+        resultsContainer.prepend(row);
+
+        if (gameOver) {
+            document.getElementById("submit").disabled = true;
+            inputElement.disabled = true;
+        }
+        let index = window.arr.findIndex(obj => obj["name"].toLowerCase() === inputElement.value.toLowerCase());
+        window.arr.splice(index, 1)[0];
+        inputElement.value = "";
+        inputElement.focus();
+        if (tries < 5) {
+            clueCountdown.innerText = `Clues in ${5 - tries} tries`
+        }
+    }
+
+    if (tries == 5) {
+        let nameClue = document.getElementById('name_clue')
+        clueCountdown.classList.add('hidden')
+        nameClue.classList.remove('hidden')
+        nameClue.innerText = answerData["abilityName"]
+        document.getElementById('splash-icon').classList.remove('grayscale')
+    }
+}
+
+// Displays congratulatory message when the correct guess is made
+function displayCongratulatoryMessage(tries) {
+    const congratsMessageElement = document.getElementById("congrats_message");
+    const triesTextElement = document.getElementById('tried-text')
+    triesTextElement.innerText = tries < 2 ? "You guessed it in 1 try!" : `You guessed it in ${tries} tries!`;
+    setTimeout(() => {
+        congratsMessageElement.classList.remove("hidden");
+        congratsMessageElement.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+    }, 500); // Delay
+}
+
+// submit on enter
+function checkSubmit(e) {
+    if(e && e.keyCode == 13) {
+        e.preventDefault();
+        document.getElementById('submit').click()
+    }
+}
+
+// Load data and initialize event listeners on window load
+window.addEventListener('load', async function() {
+    // Fetch today's answer
+    const answerRes = await fetch("/static/answers/spy/todays_answer.json");
+    answerData = await answerRes.json();  // Load today's answer
+    console.log(answerData);
+    
+    // Fetch character info data
+    const characterInfoRes = await fetch("/static/data/classicModeInfo.json");
+    charactersInfoData = await characterInfoRes.json();  // Load all character info
+    
+    // Display today's answer splash image instead of ability icon
+    const splashElement = document.getElementById('splash-icon');
+    let splashImageName = answerData["id"].toLowerCase().replace(/\s+/g, '-');
+    splashElement.style.backgroundImage = `url('/static/images/character_splashes/${splashImageName}_splash.png')`;
+    splashElement.style.backgroundSize = "16px 16px;"
+    splashElement.style.backgroundPosition = "center";
+    splashElement.style.imageRendering = "pixelated";  // Apply pixelated rendering
+
+    // Focus the guess input and add event listeners
+    document.getElementById('guess').focus();
+    document.addEventListener("keyup", checkSubmit);
+    document.getElementById("guess-form").addEventListener("submit", submitGuess);
+});
