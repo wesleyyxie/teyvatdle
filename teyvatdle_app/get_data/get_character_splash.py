aa5ac18d3ebf2d3bffc941611bbc746b2ae4e6ef
wesleyyxie <wesleyxie05@gmail.com>
2024-11-06 23:16:59 -0500

    made all splash art 1 by 1 aspect ratio and created all pixelated versions

--- teyvatdle_app/get_data/get_character_splash.py
+++ teyvatdle_app/get_data/get_character_splash.py
@@ -6,50 +6,6 @@ from io import BytesIO
 from bs4 import BeautifulSoup
 
 
-def get_icons(info):
-    cwd = os.getcwd()
-    path_to_splash_folder = os.path.join(
-        cwd, "teyvatdle_app", "static", "images", "character_splashes"
-    )
-    for c in info:
-        id = c.get("id").lower()
-        print(id)
-        no_icons = [
-            "alhaitham",
-            "baizhu",
-            "clorinde",
-            "collei",
-            "dehya",
-            "dori",
-            "faruzan",
-            "kachina",
-            "kaveh",
-            "kinich",
-            "kirara",
-            "kuki-shinobu",
-            "layla",
-            "mika",
-            "mualani",
-            "nahida",
-            "nilou",
-            "sethos",
-            "shikanoin-heizou",
-            "sigewinne",
-            "thoma",
-            "tighnari",
-            "yaoyao",
-        ]  # These needs to be manually retrieved
-        if id not in no_icons:
-            try:
-                response_burst = requests.get(
-                    f"https://genshin.jmp.blue/characters/{id}/gacha-splash"
-                )
-                splash = Image.open(BytesIO(response_burst.content))
-                splash.save(os.path.join(path_to_splash_folder, f"{id}_splash.png"))
-            except:
-                print(f"no {id}")
-
-
 def get_splashes(info):
     cwd = os.getcwd()
     path_to_splash_folder = os.path.join(
@@ -74,10 +30,20 @@ def get_splashes(info):
 
         print(url)
         response_burst = requests.get(url, timeout=30)
-        splash = Image.open(BytesIO(response_burst.content))
+        splash = Image.open(BytesIO(response_burst.content)).convert("RGBA")
+        original_width, original_height = splash.size
+
+        if original_height > original_width:
+            new_dimension = original_height
+        else:
+            new_dimension = original_width
+
+        new_splash = Image.new("RGBA", (new_dimension, new_dimension), (0, 0, 0, 0))
+        position = ((new_dimension - original_width) // 2, (new_dimension - original_height) // 2)
+        new_splash.paste(splash, position, splash)
         for c in info:
             if c.get("name").lower().replace(" ", "_") == character_name.lower():
-                splash.save(
+                new_splash.save(
                     os.path.join(
                         path_to_splash_folder, f"{c.get("id").lower()}_splash.png"
                     )
@@ -104,4 +70,4 @@ if __name__ == "__main__":
     path_to_character_info = "./teyvatdle_app/static/data/classicModeInfo.json"
     with open(path_to_character_info) as character_info:
         info = json.load(character_info)
-        check_if_all_in_folder(info)
+        get_splashes(info)
---
+++ teyvatdle_app/get_data/pixelate_splash_arts.py
@@ -0,0 +1,18 @@
+import os
+from PIL import Image
+
+def pixelate_images():
+    path_to_splash = "./teyvatdle_app/static/images/character_splashes"
+    files = files = [i for i in os.listdir(path_to_splash) if not os.path.isdir(os.path.join(path_to_splash, i))]
+    print(files)
+    for f in files[32:]:
+        print(f)
+        splash = Image.open(path_to_splash + "/" + f)
+        for i in range(1, 4):
+            splash_small = splash.resize((i * 16, i*16), resample=Image.Resampling.BILINEAR)
+            splash_small = splash_small.resize(splash.size, Image.Resampling.NEAREST)
+            splash_small.save(f'{path_to_splash}/pixelated/{f.split(".png")[0]}_pixelated_{i}.png')
+
+    
+if __name__ == "__main__":
+    pixelate_images()
