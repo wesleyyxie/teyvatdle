4931965a849ce82f986b3055a8c61f3676f2114e
wesleyyxie <wesleyxie05@gmail.com>
2024-11-18 20:33:11 -0500

    fixed voicelines mode

--- teyvatdle_app/get_answers/update_voiceline_answer.py
+++ teyvatdle_app/get_answers/update_voiceline_answer.py
@@ -11,10 +11,15 @@ def get_random_character():
         current_directory, "..", "static", "data", "voicelines.json"
     )
     previous_answers_path = os.path.join(
-        current_directory, "..", "static", "answers","voiceline", "previous_answers.json"
+        current_directory,
+        "..",
+        "static",
+        "answers",
+        "voiceline",
+        "previous_answers.json",
     )
     with open(all_characters_path, "r") as characters_json:
-            characters = json.load(characters_json)
+        characters = json.load(characters_json)
 
     with open(previous_answers_path, "r") as previous_answers_json:
         try:
@@ -23,7 +28,6 @@ def get_random_character():
                 previous_answers = []
         except JSONDecodeError:
             previous_answers = []
-    
 
     while True:
         random_character = random.choice(characters)
@@ -43,18 +47,23 @@ def update_voiceline_answer():
     random_character = (
         get_random_character()
     )  # Call the function to get a random character
-    
+
     # Serializing json
     character_json = json.dumps(random_character, indent=4)
     # Writing to sample.json
     with open(
         os.path.join(
-            current_directory, "..", "static", "answers", "voiceline", "todays_answer.json"
+            current_directory,
+            "..",
+            "static",
+            "answers",
+            "voiceline",
+            "todays_answer.json",
         ),
         "w",
     ) as outfile:
         outfile.write(character_json)
-    
-#if __name__ == "__main__":
-#    update_voiceline_answer()
 
+
+# if __name__ == "__main__":
+#    update_voiceline_answer()
