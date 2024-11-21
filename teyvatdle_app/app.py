0c85a8d5b6a43de65377717d476f1a18e9139595
wesleyyxie <wesleyxie05@gmail.com>
2024-11-21 14:48:38 -0500

    added timer and utc timezone for answer change

--- teyvatdle_app/app.py
+++ teyvatdle_app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
+import datetime
 
 from get_answers.update_classic_answer import update_classic_answer
 from get_answers.update_voiceline_answer import update_voiceline_answer
@@ -10,6 +11,7 @@ from get_answers.update_spy_answer import update_spy_answer
 app = Flask(__name__)
 # IF DEBUG MODE IS ON, IT WILL TRIGGER THE SCHEDULER AGAIN
 
+
 def update_answers():
     print("Updating answers...")
     update_classic_answer()
@@ -17,17 +19,26 @@ def update_answers():
     update_ability_answer()
     update_spy_answer()
 
+
 def scheduler():
     print("running scheduler")
     scheduler = BackgroundScheduler()
     trigger = CronTrigger(
-        year="*", month="*", day="*", hour="*", minute="*", second="24"
+        year="*",
+        month="*",
+        day="*",
+        hour="*",
+        minute="*",
+        second="10",
+        timezone=datetime.UTC,
     )
     scheduler.add_job(func=update_answers, trigger=trigger)
     scheduler.start()
 
-#scheduler()
+
+# scheduler()
 update_answers()
+
 
 @app.route("/spy")
 def spy():
@@ -55,4 +66,4 @@ def hello_world():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
